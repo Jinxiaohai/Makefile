38589d238222a4049fb9a1f2c7c2c657455be60e
xiaohai <xiaohaijin@outlook.com>
2017-05-18 15:37:10 +0800

    shared lib

---
+++ libso/include/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+void shuchu();
+
+#endif /* HELLO_H */
---
+++ libso/src/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+using namespace std;
+
+void shuchu()
+{
+  cout << "Hello World." << endl;
+}
